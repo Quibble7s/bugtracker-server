60e129fbdb02fb6e1822cd979909ab8ea3e16f01
Cesar Gallardo <wuelo789@gmail.com>
2022-05-12 20:14:34 -0500

    Removed dead code.

--- Repositories/Auth/AuthRepo.cs
+++ Repositories/Auth/AuthRepo.cs
@@ -1,16 +1,8 @@
-﻿using bugtracker.Config;
-using bugtracker.Lib;
+﻿using bugtracker.Lib;
 using bugtracker.Lib.Jwt;
 using bugtracker.Models;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 using BCryptNet = BCrypt.Net.BCrypt;
 
@@ -18,13 +10,11 @@ namespace bugtracker.Repositories {
 	public class AuthRepo : IAuthRepo {
 
 		private readonly IJwtUtils jwtUtils;
-		private readonly IConfiguration configuration;
 		private readonly IMongoCollection<User> userCollection;
 		private readonly FilterDefinitionBuilder<User> userFilter = Builders<User>.Filter;
 
-		public AuthRepo(IMongoClient client, IConfiguration configuration, IJwtUtils jwtUtils) {
+		public AuthRepo(IMongoClient client, IJwtUtils jwtUtils) {
 			this.jwtUtils = jwtUtils;
-			this.configuration = configuration;
 			IMongoDatabase database = client.GetDatabase(DBNames.DB_NAME);
 			userCollection = database.GetCollection<User>(DBNames.DB_USERS);
 		}
@@ -37,18 +27,6 @@ namespace bugtracker.Repositories {
 			bool verified = BCryptNet.Verify(password, user.Password);
 			if (!verified)
 				return null;
-
-			var tokenHandler = new JwtSecurityTokenHandler();
-			var tokenKey = Encoding.ASCII.GetBytes(configuration.GetSection(nameof(JwtConfig)).Get<JwtConfig>().Secret);
-
-			var tokenDescriptor = new SecurityTokenDescriptor() { 
-				Subject = new ClaimsIdentity(new Claim[] { 
-					new Claim(ClaimTypes.Name, user.Id.ToString()),
-					new Claim(ClaimTypes.Role, user.Role),
-				}),
-				Expires = DateTime.UtcNow.AddHours(1),
-				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
-			};
 
 			var token = jwtUtils.GenerateToken(user);
 
